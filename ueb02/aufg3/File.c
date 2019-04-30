af0778d102707d74831ca617dd9c542cc0215ac8
Nico <spaniernico@gmail.com>
2019-04-30 18:25:43 +0200

    changed file

--- ueb02/aufg3/File.c
+++ ueb02/aufg3/File.c
@@ -1,11 +1,57 @@
 #include<stdio.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
 
-int main(int numberOfArguments, char* arguments[]) {
-	int i;
-	printf("Anzahl Argumente: %d\n", numberOfArguments);	
-	for(i=0; i < numberOfArguments; i++) {
-		printf("arg[%d]: %s\n", i, arguments[i]);
-	}
-	return 0;
+int processFile(char file[]);
+
+int zeit(time_t aktuelleZeit);
+
+int main(int number, char** arguments) {
+        int i;
+        for(int i = 0; i < number; i++) {
+                if(i == 0)
+                        continue;
+                processFile(arguments[i]);
+        }
+        return 0;
+}
+
+int processFile(char fileString[]) {
+        int file = 0;
+        file = open(fileString, O_RDONLY);
+        struct stat fileStat;
+
+        int i = lstat(fileString, &fileStat);
+
+        if(i != 0) {
+                        printf("Diese Datei existiert nicht!");
+                        return -1;
+                }
+
+        printf("Mode:                     %lo (octal)\n",
+            (unsigned long) fileStat.st_mode);
+                zeit( fileStat.st_atime );
+}
+
+int zeit(time_t aktuelleZeit){
+    struct tm *zeitStruct;
+    zeitStruct = localtime(&aktuelleZeit);
+
+    time(&aktuelleZeit);
+    char *stringTokens = strtok(ctime(&aktuelleZeit)," ");
+    int i = 0;
+
+    while(stringTokens != NULL){
+        if(i == 4){
+            printf("%s ",zeitStruct->tm_zone);
+        } else {
+            printf("%s ",stringTokens);
+            stringTokens = strtok(NULL," ");
+        }
+        i = i+1;
+    }
 }
